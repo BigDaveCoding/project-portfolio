3e5000c69b6eedeab825bccf7548996158b3b06a
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-27 13:28:46 +0000

    added an eventlistener to the nav button which toggles the class hidden on the nav menu making it appear/disappear

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 
 const nav_button = document.querySelector('.nav_button')
 console.log(nav_button)
+const nav_menu = document.querySelector('.nav_menu')
+console.log(nav_menu)
+
+nav_button.addEventListener('click', () => {
+    nav_menu.classList.toggle('hidden')
+});
