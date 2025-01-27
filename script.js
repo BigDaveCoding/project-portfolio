3f88a434edee0072d7d2a761f49c67561d8c91e5
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-27 13:48:34 +0000

    added nav_link class to nav links. Accessing these and storing them in nav_links var. for each link im adding an event listener which adds hidden to the navmenu when an a link is clicked

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ console.log(nav_menu)
 nav_button.addEventListener('click', () => {
     nav_menu.classList.toggle('hidden')
 });
+
+const nav_links = document.querySelectorAll('.nav_link')
+console.log(nav_links)
+
+nav_links.forEach(link => {
+    link.addEventListener('click', () => {
+        if (!nav_menu.classList.contains('hidden')) {
+            nav_menu.classList.add('hidden')
+        }
+    })
+});
